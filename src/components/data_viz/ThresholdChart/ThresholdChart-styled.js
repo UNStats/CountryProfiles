2d11294bdeb3c7432a7a18fff8e432087cbb8d77
Cody Lawson <CLawson@esri.com>
2019-06-28 17:49:39 -0600

    Style threshold chart

---
+++ src/components/data_viz/ThresholdChart/ThresholdChart-styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledChart = styled.div`
+  flex: 1 0 auto;
+  height: 120px;
+`;
+
+export { StyledChart };
--- src/components/data_viz/ThresholdChart/ThresholdChart.js
+++ src/components/data_viz/ThresholdChart/ThresholdChart.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { StyledChart } from './ThresholdChart-styled';
 import {
   StyledTimeSeries,
   StyledDescriptionContainer,
   StyledDescriptionText,
-  StyledChartSection,
-  StyledChart
+  StyledChartSection
   // StyledCrosshairTooltip,
   // StyledCrosshairLabel,
   // StyledCrosshairValue
@@ -19,9 +19,10 @@ import AutoSizer from 'react-virtualized-auto-sizer';
 import {
   XYPlot,
   XAxis,
-  // YAxis,
+  YAxis,
   HorizontalBarSeries,
-  CustomSVGSeries
+  CustomSVGSeries,
+  GradientDefs
 } from 'react-vis';
 import 'react-vis/dist/style.css';
 
@@ -30,8 +31,8 @@ class ThresholdChart extends Component {
     return (
       <g transform="translate(0,20)">
         <polygon
-          points="7,0 7,-500 13,-500 13,0 27,22 -7,22"
-          fill="rgb(100,100,100)"
+          points="-3,-5 -3,-50 3,-50 3,-5 13,10 -13,10"
+          fill="rgb(180,185,190)"
           stroke="white"
           strokeWidth={3}
         />
@@ -40,34 +41,59 @@ class ThresholdChart extends Component {
   }
 
   getThresholdChart = ({ series, goalInfo }) => {
+    let highVal = Number(series.threshold) * 1.1;
+    if (series.data_numeric_part[0] > Number(series.threshold)) {
+      highVal = series.data_numeric_part[0] * 1.1;
+    }
+
     return (
       <AutoSizer>
         {({ width }) => (
           <XYPlot
             animation={{ duration: 5000 }}
             width={width}
-            height={100}
-            yPadding={50}
+            height={125}
             stackBy="x"
           >
+            <GradientDefs>
+              <linearGradient id="greenGradient" x1="0" x2="1" y1="0" y2="0">
+                <stop offset="0%" stopColor="rgb(59,170,75)" />
+                <stop offset="100%" stopColor="rgb(171,206,84)" />
+              </linearGradient>
+              <linearGradient id="redGradient" x1="0" x2="1" y1="0" y2="0">
+                <stop offset="0%" stopColor="rgb(231,126,94)" />
+                <stop offset="100%" stopColor="rgb(228,83,70)" />
+              </linearGradient>
+            </GradientDefs>
             <HorizontalBarSeries
-              color="green"
-              data={[{ y: 2, x: Number(series.threshold) }]}
+              barWidth={0.5}
+              color={'url(#greenGradient)'}
+              data={[{ y: 1, x: Number(series.threshold) }]}
             />
             <HorizontalBarSeries
-              color="red"
+              barWidth={0.5}
+              color={'url(#redGradient)'}
               data={[
                 {
-                  y: 2,
-                  x: Number(series.threshold) * 0.1
+                  y: 1,
+                  x: highVal - Number(series.threshold)
                 }
               ]}
             />
             <CustomSVGSeries
               customComponent={this.getSVGThingy}
-              data={[{ x: 5, y: 1 }]}
+              data={[{ x: series.data_numeric_part[0], y: 1 }]}
             />
             <XAxis />
+            <YAxis
+              hideLine
+              style={{
+                ticks: { stroke: 'transparent' }
+              }}
+              left={8}
+              tickValues={[1]}
+              tickFormat={i => series.fact_years[0]}
+            />
           </XYPlot>
         )}
       </AutoSizer>
