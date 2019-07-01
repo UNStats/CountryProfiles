59428a1189c3c720523a38430c2580de89cdb205
Cody Lawson <CLawson@esri.com>
2019-07-01 11:16:32 -0600

    Style embed dialog and button

--- src/components/EmbedHelper/EmbedHelper-styled.js
+++ src/components/EmbedHelper/EmbedHelper-styled.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
-import { CalciteH2 } from 'calcite-react/Elements';
+import { CalciteH2, CalciteP } from 'calcite-react/Elements';
 import Panel from 'calcite-react/Panel';
 import { ModalActions } from 'calcite-react/Modal';
+import { FormControl } from 'calcite-react/Form';
+
+import { unitCalc } from 'calcite-react/utils/helpers';
 
 const StyledDimensionsContainer = styled.div`
   display: flex;
@@ -9,6 +12,11 @@ const StyledDimensionsContainer = styled.div`
 
 const StyledModalHeader = styled(CalciteH2)`
   margin-bottom: 0.5rem;
+`;
+
+const StyledSubheader = styled(CalciteP)`
+  color: ${props => props.theme.palette.darkerGray};
+  margin-bottom: ${props => unitCalc(props.theme.baseline, 2, '/')};
 `;
 
 const StyledPanel = styled(Panel)`
@@ -19,9 +27,16 @@ const StyledModalActions = styled(ModalActions)`
   margin-top: 1.55rem;
 `;
 
+const StyledSmallFormControl = styled(FormControl)`
+  margin: 0;
+  margin-right: ${props => unitCalc(props.theme.baseline, 2, '/')};
+`;
+
 export {
   StyledDimensionsContainer,
   StyledModalHeader,
+  StyledSubheader,
   StyledPanel,
-  StyledModalActions
+  StyledModalActions,
+  StyledSmallFormControl
 };
--- src/components/GoalHeader/GoalHeader.js
+++ src/components/GoalHeader/GoalHeader.js
@@ -1,32 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {
   StyledGoalHeader,
+  StyledHeaderContent,
   StyledIconContainer,
-  StyledContentContainer,
+  StyledTextContainer,
   StyledHeaderText,
-  StyledSubheader
+  StyledSubheader,
+  StyledActionButton
 } from './GoalHeader-styled';
 
-const GoalHeader = ({ goal, goalInfo }) => {
-  return (
-    <StyledGoalHeader noBorder noPadding white>
-      <StyledIconContainer>
-        <img
-          src={`https://s3.amazonaws.com/un-country-profiles-2019/SDG_ICONS_2019/E_SDG+goals_icons-individual-rgb-${
-            goal.goalCode
-          }.png`}
-          alt="goal icon"
+import EmbedHelper from '../EmbedHelper';
+
+import Tooltip from 'calcite-react/Tooltip';
+import CodeIcon from 'calcite-ui-icons-react/CodeIcon';
+
+class GoalHeader extends Component {
+  state = {
+    showEmbedHelper: false
+  };
+
+  getEmbedBtn = showEmbedBtn => {
+    if (showEmbedBtn) {
+      return (
+        <Tooltip title="Embed" placement="left">
+          <StyledActionButton
+            iconButton
+            icon={<CodeIcon />}
+            onClick={this.showEmbedHelper}
+          />
+        </Tooltip>
+      );
+    }
+  };
+
+  getEmbedHelper = () => {
+    if (this.state.showEmbedHelper) {
+      return (
+        <EmbedHelper
+          goal={this.props.goal}
+          goalInfo={this.props.goalInfo}
+          countryCode={this.props.countryCode}
+          onClose={this.hideEmbedHelper}
         />
-      </StyledIconContainer>
-      <StyledContentContainer>
-        <StyledHeaderText>{goalInfo.short}</StyledHeaderText>
-        <StyledSubheader>{goalInfo.title}</StyledSubheader>
-      </StyledContentContainer>
-    </StyledGoalHeader>
-  );
-};
+      );
+    }
+  };
+
+  showEmbedHelper = () => {
+    this.setState({ showEmbedHelper: true });
+  };
+
+  hideEmbedHelper = () => {
+    this.setState({ showEmbedHelper: false });
+  };
+
+  render() {
+    const { goal, goalInfo } = this.props;
+    const params = new URLSearchParams(window.location.search);
+    const showEmbedBtn = !params.get('goal');
+
+    return (
+      <StyledGoalHeader noBorder noPadding white>
+        <StyledHeaderContent>
+          <StyledIconContainer>
+            <img
+              src={`https://s3.amazonaws.com/un-country-profiles-2019/SDG_ICONS_2019/E_SDG+goals_icons-individual-rgb-${
+                goal.goalCode
+              }.png`}
+              alt="goal icon"
+            />
+          </StyledIconContainer>
+          <StyledTextContainer>
+            <StyledHeaderText>{goalInfo.short}</StyledHeaderText>
+            <StyledSubheader>{goalInfo.title}</StyledSubheader>
+          </StyledTextContainer>
+        </StyledHeaderContent>
+        {this.getEmbedBtn(showEmbedBtn)}
+        {this.getEmbedHelper()}
+      </StyledGoalHeader>
+    );
+  }
+}
 
 GoalHeader.propTypes = {
   goal: PropTypes.object.isRequired
