37c960c190394773597a58d57f3cd87f9d2e5b94
Joshua Peterson <>
2019-07-03 16:34:14 -0600

    temporarily remove country maps

--- src/components/CountryHeader/CountryMap.js
+++ src/components/CountryHeader/CountryMap.js
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 
 import { StyledImageCard } from './CountryHeader-styled';
 
+// TODO: this is a placeholder
+import UgandaMap from '../../assets/uganda_map.png';
+
 class CountryMap extends Component {
-  state = { image: null };
+  state = { image: UgandaMap };
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.code !== this.props.code) {
-      this.getCountryImage();
-    }
-  }
+  // async componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.code !== this.props.code) {
+  //     this.getCountryImage();
+  //   }
+  // }
 
-  async componentDidMount() {
-    this.getCountryImage();
-  }
+  // async componentDidMount() {
+  //   this.getCountryImage();
+  // }
 
-  getCountryImage = async () => {
-    const { default: image } = await import(
-      `../../assets/countryThumbnails/${this.props.code}.png`
-    );
-    this.setState({ image });
-  };
+  // getCountryImage = async () => {
+  //   const { default: image } = await import(
+  //     `../../assets/countryThumbnails/${this.props.code}.png`
+  //   );
+  //   this.setState({ image });
+  // };
 
   render() {
     if (!this.state.image) {
